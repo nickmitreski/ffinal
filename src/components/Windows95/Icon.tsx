d5147e98f6fe9fbe96afa951cbb1de24546e7441
nickmitreski <85538161+nickmitreski@users.noreply.github.com>
2025-06-29 15:22:32 +1000

    Fix Windows 95 Desktop Icons

--- src/components/Windows95/Icon.tsx
+++ src/components/Windows95/Icon.tsx
@@ -10,11 +10,18 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({ id, name, icon, x, y, onOpen }) => {
+  const handleDoubleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onOpen();
+  };
+
   return (
     <div 
       className="win95-desktop-icon" 
       style={{ left: `${x}px`, top: `${y}px` }}
-      onDoubleClick={onOpen}
+      onDoubleClick={handleDoubleClick}
+      data-icon-id={id}
     >
       <img src={icon} alt={name} className="win95-desktop-icon-img" />
       <div className="win95-desktop-icon-text">{name}</div>
